57fd122f6547b3385d43232702afeab015a5aa71
Nathaniel Brassell <nathaniel303@gmail.com>
2014-01-14 14:29:55 -0500

    Update and rename Code.txt to Code.java

--- Code.java
+++ Code.java
@@ -1,15 +1,21 @@
-Code
+public class Game{
 
-public class Player{
-
-int position;
+int position = 0;
 String piece;
 int money = 3500;
 Arraylist properties = new ArrayList;
 
-public int roll(){
+public int rollDice(){
 int rand = (int)(Math.random() * 6) + 1;
 int rand2 = (int)(Math.random() * 6) + 1;
 int rand3 = rand + rand2;
 return rand3;
+}
+
+public void Move{
+
+public void main(String [] args){
+
+for (int x = 0; x < 5; x++){
+spaces.add(x);
 }
