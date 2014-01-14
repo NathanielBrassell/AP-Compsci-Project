37eda17ef3109871154a90ca5842e9fd875f28d3
Nathaniel Brassell <nathaniel303@gmail.com>
2014-01-14 14:47:08 -0500

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,21 +1,43 @@
-public class Game{
+public class Code{
 
-int position = 0;
-String piece;
-int money = 3500;
-Arraylist properties = new ArrayList;
+ 	int position;
+  String piece;
+  String type;
+  int money;
+  int price;
+  int rent;
+  Arraylist propertiesOwned = new ArrayList();
+  Arraylist board = new Arraylist();
+  public final static Arraylist properties = new Arraylist;
+  
+  public Code(String typ, String pece, int pric, int pos, int ren){
+  	type = typ;
+  	
+  	if ( type.equals("Prop")){
+  		price = pric;
+  		rent = ren;
+  	}
+  	
+  	else(){
+  		piece = pece;
+  		money = 20;
+  		position = pos;
+  	}
+  }
 
-public int rollDice(){
-int rand = (int)(Math.random() * 6) + 1;
-int rand2 = (int)(Math.random() * 6) + 1;
-int rand3 = rand + rand2;
-return rand3;
-}
+  public int rollDice(){
+    int rand = (int)(Math.random() * 6) + 1;
+    int rand2 = (int)(Math.random() * 6) + 1;
+    int rand3 = rand + rand2;
+    return rand3;
+  }
 
 public void Move{
 
 public void main(String [] args){
 
-for (int x = 0; x < 5; x++){
-spaces.add(x);
+  for (int x = 0; x < 17; x++){
+  	int temp_price = x + 1;
+  	int temp_rent = x + 2;
+  	
 }
