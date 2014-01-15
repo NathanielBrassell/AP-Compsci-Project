93cc4386d63fd7fd68d8ab786a69b06b5e57ceb8
Nathaniel Brassell <nathaniel303@gmail.com>
2014-01-15 14:37:48 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,4 +6,24 @@ public class Player{
 	int position = 0;
 	String name = "";
 	String piece = "";
-	Arraylist propOwned = new Arraylist
+	Arraylist propOwned = new Arraylist();
+	
+	public int rollDice(){
+		int rand1 = (int)(Math.random() * 6) + 1;
+		int rand2 = (int)(Math.random() * 6) + 1;
+		int rand3 = rnad1 + rand2;
+		return rand3;
+	}
+	
+	public Object move(Arraylist board){
+		int position += rollDice();
+		ret = board[position];
+		canBuy(ret);
+		return ret;
+	}
+	
+	public Boolean canBuy(Property prop){
+		if (money >= prop.getRent());
+	}
+	
+	
