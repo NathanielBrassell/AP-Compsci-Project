6553e40cc7ffe4a3ac77ba64cee3a437541bceca
Nathaniel Brassell <nathaniel303@gmail.com>
2014-01-16 14:14:01 -0500

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,1 +1,19 @@
 public class Property{
+  
+ private int price 0 ;
+ private int rent = 0;
+ private String name = "";
+ private Boolean Avail = true;
+ 
+ public int getPrice(){
+   return price;
+ }
+ 
+ public int getRent(){
+   return rent;
+ }
+ 
+ public String toString(){
+   return name + "costs" + price + "and rents for" + rent;
+ }
+}
