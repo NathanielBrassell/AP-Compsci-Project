9df7322bac0bb391f12400a9ae9637a55aefdefc
ZJackie <zjackiew@gmail.com>
2014-01-22 14:46:54 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,40 +2,24 @@ import java.util.ArrayList;
 
 public class Player{
 
-	int money = 20;
-	int position = 0;
-	String name = "";
-	String piece = "";
-	Arraylist propOwned = new Arraylist();
-	
-	public Player(String nam, String pece){
-		name = nam;
-		peice = pece;
-	}
-	
-	public int getMoney(){
-		return money;
-	}
-	
-	public void setMoney(int m){
-		money = m;
-	}
-	
-	public int rollDice(){
-		int rand1 = (int)(Math.random() * 6) + 1;
-		int rand2 = (int)(Math.random() * 6) + 1;
-		int rand3 = rnad1 + rand2;
-		return rand3;
-	}
-	
-	public Object move(Arraylist<Property> board){
-		int position += rollDice();
-		ret = board[position];
-		if (ret.getAvail() == false){
-			ret.rent(this)
-		}
-		return ret;
-	}
-	
-	public Buy(Property prop)
+        int money = 150
+        String name = "";
+        String piece = "";
+	ArrayList propOwned = new ArrayList();
+        
+        public Player(){
+        }
+        
+        public int getMoney(){
+                return money;
+        }
+        
+        public void setMoney(int m){
+                money = m;
+        }
+        
+
+
+        
+
 }
