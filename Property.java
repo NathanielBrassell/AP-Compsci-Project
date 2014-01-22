25cbe45097187eec2ad3cc16a6e2def0beee4d2e
ZJackie <zjackiew@gmail.com>
2014-01-22 14:46:39 -0500

    Update Property.java

--- Property.java
+++ Property.java
@@ -1,21 +1,31 @@
-public class Property{
+public class Property {
   
- private int price 0 ;
+ private int price = 0 ;
  private int rent = 0;
  private String name = "";
  private Boolean avail = true;
+        ArrayList board = new ArrayList();
+        board.add(new Property(0,0,null));
+        board.add(new Property(12,5,"PE"));
+        board.add(new Property(12,10, "Health"));
+        board.add(new Property(0,0,null));
+        board.add(new Property(0,0,null));
+        board.add(new Property(20,15, "Geometry"));
+        board.add(new Property(20,15, "Trigonometry"));
+        board.add(new Property(24,20, "Calculus"));
+        board.add(new Property(0,0,null));
+        board.add(new Property(28,25,"Biology"));
+        board.add(new Property(28,25,"Chemistry"));
+        board.add(new Property(31,30, "Physics"));
+	board.add(new Property(0,0,null));
+        board.add(new Property(36,35, "Intro to Compsci"));
+        board.add(new Property(40, 39, "AP Compsci"));
+ArrayList NotOwned = board;
  
  public Property(int prc, int rnt, String nam){
    price = prc;
    rent = rnt;
    name = nam;
- }
- 
- public Property(String nam, String type){
-   name = nam;
-   if (type.equals("Chance")){
-     
-   }
  }
  
  public int getPrice(){
